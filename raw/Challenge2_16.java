e0bb074d6c9ab3923c8d380b2a1950a025409f71
TwoFish <liangjunyi66@126.com>
2025-02-08 17:08:05 +0800

    这道题没有写测试代码。

---
+++ raw/Challenge2_16.java
@@ -0,0 +1,41 @@
+package raw;
+
+import java.util.ArrayList;
+
+public class Challenge2_16 {
+    public class Solution {
+
+        public static ListNode middleNode(ListNode head) {
+            ArrayList<Integer> list =new ArrayList<>();
+            ListNode t=head;
+            while(t!=null){
+                list.add(t.val);
+                t=t.next;
+            }
+            int size= list.size();
+            int location=size/2+1-1;
+            ListNode s=head;
+            for(int i=0;i<location;i++){
+                s=s.next;
+            }
+            return s;
+        }
+
+        public class ListNode {
+            public int val;
+            public ListNode next;
+
+            public ListNode() {
+            }
+
+            public ListNode(int val) {
+                this.val = val;
+            }
+
+            ListNode(int val, ListNode next) {
+                this.val = val;
+                this.next = next;
+            }
+        }
+    }
+}

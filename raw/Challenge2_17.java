2ce635a178296a2a74a4cf76619400bdeda74e83
TwoFish <liangjunyi66@126.com>
2025-02-11 15:10:19 +0800

    暴力解法

---
+++ raw/Challenge2_17.java
@@ -0,0 +1,45 @@
+package raw;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Stack;
+
+public class Challenge2_17 {
+    public class Solution {
+        public static boolean isPalindrome(ListNode head) {
+            ListNode t=head;
+            ArrayList<Integer> list=new ArrayList<>();
+            ArrayList<Integer> list2=new ArrayList<>();
+            while(t!=null){
+                list.add(t.val);
+                list2.add(t.val);
+                t=t.next;
+            }
+            Collections.reverse(list);
+            for(int i=0;i<list.size();i++){
+                if(list.get(i)==list2.get(i)){
+                    continue;
+                }
+                else return false;
+            }
+            return true;
+        }
+
+        public static class ListNode {
+            int val;
+            public ListNode next;
+
+            ListNode() {
+            }
+
+            public ListNode(int val) {
+                this.val = val;
+            }
+
+            ListNode(int val, ListNode next) {
+                this.val = val;
+                this.next = next;
+            }
+        }
+    }
+}

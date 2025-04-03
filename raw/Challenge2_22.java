9397aa86078196fa8a820ae01e65f2e9f11e5bc5
Angela Fish <1755301854@qq.com>
2025-04-03 14:52:30 +0800

    完成有序链表第一题。

---
+++ raw/Challenge2_22.java
@@ -0,0 +1,44 @@
+package raw;
+
+public class Challenge2_22 {
+    public class Solution {
+        public static ListNode deleteDuplicates(ListNode head) {
+            if (head == null) {
+                return null;
+            } else if (head.next == null) {
+                return head;
+            } else {
+                ListNode p, q;
+                p = q = head;
+                p = p.next;
+                while (p != null) {
+                    if (p.val == q.val) {
+                        q.next = p.next;
+                        p = p.next;
+                    } else {
+                        q = p;
+                        p = p.next;
+                    }
+                }
+                return head;
+            }
+        }
+
+        public static class ListNode {
+            public int val;
+            public ListNode next;
+
+            public ListNode() {
+            }
+
+            public ListNode(int val) {
+                this.val = val;
+            }
+
+            ListNode(int val, ListNode next) {
+                this.val = val;
+                this.next = next;
+            }
+        }
+    }
+}

459e15bc310f1fd6747878f032121927a8ad5245
TwoFish <liangjunyi66@126.com>
2025-07-29 15:02:27 +0800

    暂时提交

--- raw/Challenge3_5.java
+++ raw/Challenge3_5.java
@@ -1,4 +1,34 @@
 package raw;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
+
 public class Challenge3_5 {
+    public static class Solution {
+        public static boolean validateStackSequences(int[] pushed, int[] popped) {
+            int len = pushed.length + popped.length;
+            ArrayList<Boolean> list=new ArrayList<>();
+            //编写记录的位数
+            if(list.isEmpty()){
+                list.add(false);
+            }
+            Boolean flag=true;
+            while(true){
+                exeMethod(pushed,popped);
+                adding();
+            }
+        }
+
+        //把list写上下一组可能性。
+        private static void adding() {
+        }
+
+        //执行每一种可能性
+        private static void exeMethod(int[] pushed,int [] popped) {
+            Stack<Integer> stack=new Stack<>();
+
+        }
+
+    }
 }

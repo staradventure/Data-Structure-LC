a9a3c7a621410f2dcd384d1250a5f11d70d6151f
TwoFish <liangjunyi66@126.com>
2025-08-06 19:49:05 +0800

    代码基本写出来了，还得再改。

--- raw/Challenge3_5.java
+++ raw/Challenge3_5.java
@@ -8,48 +8,55 @@ public class Challenge3_5 {
     public static class Solution {
         public static boolean validateStackSequences(int[] pushed, int[] popped) {
             int len = pushed.length + popped.length;
-            ArrayList<Integer> pushed_list= new ArrayList<Integer>(Arrays.stream(pushed).boxed().collect(Collectors.toList()));
-            ArrayList<Integer> popped_list= new ArrayList<Integer>(Arrays.stream(popped).boxed().collect(Collectors.toList()));
-            ArrayList<Integer> list=new ArrayList<>();//一个比特中，0表示这一次入栈，1表示这一次出栈。
-            LinkedList<Integer> pushed_queue=new LinkedList<>(pushed_list);
-            LinkedList<Integer> popped_queue=new LinkedList<>(popped_list);
+            ArrayList<Integer> pushed_list = new ArrayList<Integer>(Arrays.stream(pushed).boxed().collect(Collectors.toList()));
+            ArrayList<Integer> popped_list = new ArrayList<Integer>(Arrays.stream(popped).boxed().collect(Collectors.toList()));
+            ArrayList<Integer> list = new ArrayList<>();//一个比特中，0表示这一次入栈，1表示这一次出栈。
+            LinkedList<Integer> pushed_queue = new LinkedList<>(pushed_list);
+            LinkedList<Integer> popped_queue = new LinkedList<>(popped_list);
             //编写记录的位数
-            if(list.isEmpty()){
+            if (list.isEmpty()) {
                 list.add(0);
             }
-            Boolean main_flag=false;
-            while(true){
-                if(exeMethod(pushed_queue,popped_queue,list)){
-                    main_flag=true;
+            Boolean main_flag = false;
+            while (true) {
+                if (exeMethod(pushed_queue, popped_queue, list)) {
+                    main_flag = true;
                     break;
-                }
-                else {
-                    adding();
+                } else {
+                    if (!adding(list, len)) {
+                        main_flag = false;
+                        break;
+                    }
                 }
             }
             return main_flag;
         }
 
         //把list写上下一组可能性。
-        private static void adding() {
-
+        private static Boolean adding(ArrayList<Integer> list, int len) {
+            if (list.size() < len) {
+                if (list.get(list.size() - 1) == 0) {
+                    list.set(list.size() - 1, 1);
+                } else {
+                    list.add(0);
+                }
+                return true;
+            } else return false;
         }
 
         //执行每一种可能性
-        private static Boolean exeMethod(LinkedList<Integer> pushed_queue,LinkedList<Integer> popped_queue,ArrayList<Integer> list) {
-            Stack<Integer> stack=new Stack<>();
-            Boolean flag=true;
-            for(int i=0;i<list.size();i++){
-                if(list.get(i)==0){//也就是说，这一次要入栈
+        private static Boolean exeMethod(LinkedList<Integer> pushed_queue, LinkedList<Integer> popped_queue, ArrayList<Integer> list) {
+            Stack<Integer> stack = new Stack<>();
+            Boolean flag = true;
+            for (int i = 0; i < list.size(); i++) {
+                if (list.get(i) == 0) {//也就是说，这一次要入栈
                     stack.push(pushed_queue.pop());
-                }
-                else if(list.get(i)==1){//也就是说，这一次要出栈
-                    int temp=stack.pop();
-                    if(temp==popped_queue.peek()){//这次的出栈序列是可以继续模拟的
+                } else if (list.get(i) == 1) {//也就是说，这一次要出栈
+                    int temp = stack.pop();
+                    if (temp == popped_queue.peek()) {//这次的出栈序列是可以继续模拟的
                         popped_queue.pop();
-                    }
-                    else{
-                        flag=false;
+                    } else {
+                        flag = false;
                         break;
                     }
                 }
---
+++ test/Challenge3_5Test.java
@@ -0,0 +1,27 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static raw.Challenge3_5.Solution.validateStackSequences;
+
+class Challenge3_5Test {
+    @Test
+    void simpleTest1(){
+        int [] pushed={1,2,3,4,5};
+        int [] popped={4,5,3,2,1};
+        Boolean actual=validateStackSequences(pushed,popped);
+        Boolean expected=true;
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void simpleTest2(){
+        int [] pushed={1,2,3,4,5};
+        int [] popped={4,3,5,1,2};
+        Boolean actual=validateStackSequences(pushed,popped);
+        Boolean expected=false;
+        assertEquals(expected,actual);
+    }
+
+}
